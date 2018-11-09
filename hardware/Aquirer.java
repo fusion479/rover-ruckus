fdf70dfd70fe6ae04eab6ae0258efe2f8f28dc3a
AnthonyLam <alam6@stuy.edu>
2018-11-08 21:03:12 -0500

    added empty aquirer class

---
+++ hardware/Aquirer.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.RoverRuckus.hardware;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.hardware.Mechanism;
+
+public class Aquirer extends Mechanism {
+
+    @Override
+    public void init(HardwareMap hwMap) {
+
+    }
+
+}
